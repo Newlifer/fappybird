0837147950fb001ec7de3a1804fb2f0edb9cba9f
newlifer <alex.newlifer@gmail.com>
2016-09-26 22:47:24 +0400

    First work

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 #include <pugixml.hpp>
 
 auto main() -> int
 {
     std::cout << "Time to fapp!" << std::endl;
+
+    const std::string filename = "data/AS_ROOM_20160925_438f8853-a978-41b2-bdd3-a4c2095a404c.XML";
+
+    std::ifstream stream(filename);
+
+    pugi::xml_document doc;
+    auto res = doc.load(stream);
+
     return 0;
 }
