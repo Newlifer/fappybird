b04364132822168815752b4eecf71c469c7279b8
aa.novozhilov <aa.novozhilov@tensor.ru>
2016-10-24 17:46:54 +0300

    Add dictionary interface

--- include/idictionary.hpp
+++ include/idictionary.hpp
@@ -1,14 +1,79 @@
 #pramga once
+#include <utility>
+#include <vector>
 
 namespace fappy {
-        template <typename StringType>
-        class dictionary
+        /*! \brief Dictionary base class.
+         */
+        template <typename StringType, typename Data>
+        class idictionary
         {
                 using string_type_t = StringType;
+                using data_t = Data;
+                
+                using value_t = std::pair<string_type_t, data_t>;
+                
+                struct node_t
+                {
+                    value_t value_;
+                    /*hash here...*/
+                };
         public:
-                dictionary()
+                idictionary()
+                {}
+                
+                virtual ~idictionary()
+                {}
+
+                virtual data_t& operator [] (const string_type_t& key) = 0;
+                virtual const data_t& operator [] (const string_type_t& key) const = 0;
+
+                virtual data_t& at(const string_type_t& key) = 0;
+                virtual const data_t& at(const string_type_t& key) const = 0;
+                
+                bool empty() const
+                {
+                    return false;
+                }
+
+                void clear()
+                {
+                    return;
+                }
+
+                template <typename Args...>
+                /*iterator type*/ emplace(Args&&... args)
+                {}
+                
+                std::size_t count() const
+                {
+                    return {};
+                }
+                
+                /*
+                iterator& find(const string_type_t& key)
+                {}
+                
+                const_iterator& find(const string_type_t& key) const
+                {}
+                
+                iterator& begin()
+                {}
+                
+                const_iterator& begin() const
+                {}
+                
+                iterator& end()
+                {}
+                
+                const_iterator& end() const
+                {}
+                 */
+
+                void rehash()
                 {}
 
         private:
+            std::vector<node_t> nodes_;
         };
 }
